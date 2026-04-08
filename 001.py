7d0dc0d8fc4817c518abf5afcf33e15ccae6e11b
harry-pikesley <95980050+harry-pikesley@users.noreply.github.com>
2026-04-08 22:24:47 +0100

    Update 001.py

--- 001.py
+++ 001.py
@@ -1,6 +1,15 @@
-total = 0
+threes = 0
+fives = 0
+fifteens = 0
 
-for i in range (1, 569000, 2):
-  total += i * i
+for i in range(1, 1000):
+  if i % 15 == 0:
+    fifteens += i
+  if i % 5 == 0:
+    fives += i
+  if i % 3 == 0:
+    threes += i
+
+total = threes + fives - fifteens
 
 print(total)
